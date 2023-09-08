0af6beb67dfaf83ad8b39a1a29fc8a070372b345
erikvella <vella.erik1@gmail.com>
2023-09-08 15:08:03 +0100

    [add] btn

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//  1  Salvare le due variabili età e km da percorrere
+
+// 2  Applicare lo sconto in base all'età dell'utente
+
+// 3  restiture graficamente il biglietto con il prezzo finale
+
+
+// 1 
+
+const outputTag = document.getElementById('output');
+const age = document.getElementById('age');
+const km = document.getElementById('km');
+const sconto20 = 0.2
+const sconto40 = 0.4
+let price = km * 0.21
+
+
+// 2
+
+console.log(dioopo);
+
+
+// 3
+
+outputTag.innerHTML = price.toFixed(2)
